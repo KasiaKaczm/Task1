b10282223456bab5b9cefdeea859c2dcdcb51199
Katarzyna Kaczmarek <116592953+KasiaKaczm@users.noreply.github.com>
2023-09-25 20:46:49 +0200

    Add files via upload

---
+++ src/test/java/stepsDefinitions/Task1Test.java
@@ -0,0 +1,12 @@
+package stepsDefinitions;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "Cucumber/Features/Features1",
+        glue="stepsDefinitions")
+
+public class Task1Test {
+}
